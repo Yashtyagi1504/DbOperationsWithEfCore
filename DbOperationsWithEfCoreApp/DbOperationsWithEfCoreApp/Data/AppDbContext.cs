a447b4311fa60fc685eef6987c32ce96a185f744
Yashtyagi1504 <yashtyagi1552@gmail.com>
2026-04-06 09:51:25 +0530

    Installed Ef Core and Setup the DbContext Class

---
+++ DbOperationsWithEfCoreApp/DbOperationsWithEfCoreApp/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿namespace DbOperationsWithEfCoreApp.Data;
+
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+}
--- DbOperationsWithEfCoreApp/DbOperationsWithEfCoreApp/Program.cs
+++ DbOperationsWithEfCoreApp/DbOperationsWithEfCoreApp/Program.cs
@@ -1,4 +1,7 @@
 
+using DbOperationsWithEfCoreApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace DbOperationsWithEfCoreApp
 {
     public class Program
@@ -8,6 +11,10 @@ namespace DbOperationsWithEfCoreApp
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("AppDb")));
+
 
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
